045d3559a23bf66e52bc7676b9c5cc0f60bd2c42
Venkata <44181523+vsramakrishnaraju@users.noreply.github.com>
2021-01-03 17:11:14 -0700

    comp

---
+++ vue/src/components/bar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="bar">
+    {{ info }}
+  </div>
+</template>
+
+<script>
+import { getAPI2 } from "@/axios";
+export default {
+  name: 'bar',
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    getAPI2
+      .get()
+      .then((response) => (this.info = response));
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue/src/components/foo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="foo">
+    <h1>head {{ info }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'foo',
+  data() {
+    return {
+      info: 'hellllllll'
+    };
+  }
+};
+</script>
+
+<style>
+</style>
